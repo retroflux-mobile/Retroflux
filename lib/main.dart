f073d943392a1992851d4782c5393a39bbd8fa8d
kaimingKAI <versuscc@gmail.com>
2022-06-22 21:53:57 -0400

    Resolved version conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:retroflux/screens/homepage_screen.dart';
 import 'package:retroflux/screens/login_method_screen.dart';
-//import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 
 Future<void> main() async {
-  '''
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  ''';
+
   runApp(const MyApp());
 }
 
