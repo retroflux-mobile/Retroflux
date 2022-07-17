7c9a8ad593f3de8a26d1ec41098f26f4c5190aac
Cusanity <wyc935398521@gmail.com>
2022-07-17 08:24:46 -0400

    Sidebar buttons

--- lib/widgets/scroller.dart
+++ lib/widgets/scroller.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:tiktoklikescroller/tiktoklikescroller.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import '../widgets/sidebar_buttons.dart';
 
 class Scroller extends StatefulWidget {
   final List<List<Widget>> widgetList;
@@ -21,7 +22,8 @@ class _ScrollerState extends State<Scroller> {
   late List<ItemScrollController> itemControllers;
 
   Future scrollToItem(int listIdx, int itemIdx) async {
-    itemControllers[listIdx].scrollTo(index: itemIdx, duration: const Duration(milliseconds: 200));
+    itemControllers[listIdx]
+        .scrollTo(index: itemIdx, duration: const Duration(milliseconds: 200));
   }
 
   @override
@@ -37,48 +39,67 @@ class _ScrollerState extends State<Scroller> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: TikTokStyleFullPageScroller(
-        contentSize: widget.widgetList.length,
-        swipePositionThreshold: 0.05,
-        // ^ the fraction of the screen needed to scroll
-        swipeVelocityThreshold: 1000,
-        // ^ the velocity threshold for smaller scrolls
-        controller: controller,
-        // ^ registering our own function to listen to page changes
-        builder: (BuildContext context, int listIdx) {
-          return ScrollablePositionedList.builder(
-            scrollDirection: Axis.horizontal,
-            itemScrollController: itemControllers[listIdx],
-            itemCount: widget.widgetList[listIdx].length,
-            itemBuilder: (BuildContext context, int itemIdx) {
-              return GestureDetector(
-                onHorizontalDragUpdate: (DragUpdateDetails details) {
-                  if (details.primaryDelta! < 0.0) {
-                    scrollToItem(listIdx, min(widget.widgetList[listIdx].length - 1, itemIdx + 1));
-                  }
-                  else {
-                    scrollToItem(listIdx, max(0, itemIdx - 1));
-                  }
-                },
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: double.infinity,
-                    child: widget.widgetList[listIdx][itemIdx],
+    return TikTokStyleFullPageScroller(
+      contentSize: widget.widgetList.length,
+      swipePositionThreshold: 0.05,
+      // ^ the fraction of the screen needed to scroll
+      swipeVelocityThreshold: 1000,
+      // ^ the velocity threshold for smaller scrolls
+      controller: controller,
+      // ^ registering our own function to listen to page changes
+      builder: (BuildContext context, int listIdx) {
+        Widget sidebarButtons = SidebarButtons(
+          isFavorite: false,
+          onAvatar: () {},
+          onFavorite: () {},
+          onComment: () {},
+          onShare: () {},
+        );
+        return Stack(
+          children: <Widget>[
+            ScrollablePositionedList.builder(
+              scrollDirection: Axis.horizontal,
+              itemScrollController: itemControllers[listIdx],
+              itemCount: widget.widgetList[listIdx].length,
+              itemBuilder: (BuildContext context, int itemIdx) {
+                return GestureDetector(
+                  onHorizontalDragUpdate: (DragUpdateDetails details) {
+                    if (details.primaryDelta! < 0.0) {
+                      scrollToItem(
+                          listIdx,
+                          min(widget.widgetList[listIdx].length - 1,
+                              itemIdx + 1));
+                    } else {
+                      scrollToItem(listIdx, max(0, itemIdx - 1));
+                    }
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: widget.widgetList[listIdx][itemIdx],
+                    )
                   )
-                )
-              );
-            },
-          );
-        },
-      ),
+                );
+              },
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.bottomRight,
+              child: sidebarButtons,
+            )
+          ],
+        );
+      },
     );
   }
 
-  void _handleCallbackEvent(ScrollDirection direction, ScrollSuccess success,{int? currentIndex}) {
-    print("Scroll callback received with data: {direction: $direction, success: $success and index: ${currentIndex ?? 'not given'}}");
+  void _handleCallbackEvent(ScrollDirection direction, ScrollSuccess success,
+      {int? currentIndex}) {
+    print(
+        "Scroll callback received with data: {direction: $direction, success: $success and index: ${currentIndex ?? 'not given'}}");
   }
 }
